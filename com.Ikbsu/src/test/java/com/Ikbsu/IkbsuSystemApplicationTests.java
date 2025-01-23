1f02dd0f39293a27241b51b5781880add9fb822d
Hasan kirmani <hassankimrani930@gmail.com>
2025-01-23 19:06:38 +0500

    project initialized

---
+++ com.Ikbsu/src/test/java/com/Ikbsu/IkbsuSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Ikbsu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IkbsuSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
