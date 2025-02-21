007a4586ceaf215b93a79b777364b35e03943fd1
Hasan kirmani <Hassankimrani930@gmail.com>
2025-02-21 18:27:57 +0500

    Added CustomResponse DTO

--- com.Ikbsu/src/main/java/com/Ikbsu/controller/AuthController.java
+++ com.Ikbsu/src/main/java/com/Ikbsu/controller/AuthController.java
@@ -2,12 +2,16 @@ package com.Ikbsu.controller;
 
 
 import com.Ikbsu.dto.AuthRequest;
+import com.Ikbsu.dto.CustomResponse;
+import com.Ikbsu.dto.ErrorResponse;
 import com.Ikbsu.jwt.JwtUtil;
 import com.Ikbsu.model.User;
 import com.Ikbsu.service.CustomUserDetailsService;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -16,6 +20,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/auth")
@@ -31,12 +37,30 @@ public class AuthController {
     private JwtUtil jwtUtil;
 
     @PostMapping("/login")
-    public String login(@RequestBody AuthRequest loginRequest) {
-        Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword())
-        );
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-        UserDetails userDetails = userDetailsService.loadUserByUsername(loginRequest.getUsername());
-        return jwtUtil.generateToken(userDetails);  // Return the JWT token
+//    public String login(@RequestBody AuthRequest loginRequest) {
+//        Authentication authentication = authenticationManager.authenticate(
+//                new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword())
+//        );
+//        SecurityContextHolder.getContext().setAuthentication(authentication);
+//        UserDetails userDetails = userDetailsService.loadUserByUsername(loginRequest.getUsername());
+//        return jwtUtil.generateToken(userDetails);  // Return the JWT token
+//    }
+    public ResponseEntity<CustomResponse<String>> login(@RequestBody AuthRequest loginRequest) {
+        try {
+            Authentication authentication = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword())
+            );
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            UserDetails userDetails = userDetailsService.loadUserByUsername(loginRequest.getUsername());
+            String token = jwtUtil.generateToken(userDetails);
+
+            CustomResponse<String> response = new CustomResponse<>(token, null);
+            return ResponseEntity.ok(response);
+        } catch (BadCredentialsException e) {
+            List<ErrorResponse> errors = List.of(new ErrorResponse("INVALID_CREDENTIALS","Invalid username/password"));
+            CustomResponse<String> response = new CustomResponse<>(null, errors);
+            return ResponseEntity.status(HttpServletResponse.SC_UNAUTHORIZED).body(response);
+        }
     }
+
 }
---
+++ com.Ikbsu/src/main/java/com/Ikbsu/dto/CustomResponse.java
@@ -0,0 +1,18 @@
+package com.Ikbsu.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CustomResponse<T>{
+
+    private T body;
+    private List<ErrorResponse> errors;
+}
